1a7381bd0f563b30b1cc5df9a3e4a959e0f85af0
naymoreno <nadineemoreno@gmail.com>
2026-07-17 00:42:32 -0700

    nudge notifications

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clients) => {
+      if (clients.length) return clients[0].focus();
+      return self.clients.openWindow('./index.html');
+    })
+  );
+});
